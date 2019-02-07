8b4eef94ca82506f430e38e1d50a3f0182b21fb3
Julien <jc@sytadelle.fr>
2019-02-07 15:31:44 +0100

    added FormElement.jsx

--- components/forms/FormElement.jsx
+++ components/forms/FormElement.jsx
@@ -1,13 +1,4 @@
 import { Form } from 'react-bootstrap';
-//import React, { PureComponent } from 'react';
 import { registerComponent } from 'meteor/vulcan:core';
 
-/*
-class Form extends PureComponent {
-
-    render () {
-        return (<form>{childen}</form>)
-    }
-}
-*/
 registerComponent('FormElement', Form);
